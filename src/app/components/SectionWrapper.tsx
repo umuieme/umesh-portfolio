753920a9fe3e4b2a24a31ed52393f0fe70c5096c
Umesh Basnet <basnet0umesh@gmail.com>
2025-06-24 15:18:01 -0400

    -[x] fix hero section

--- src/app/components/SectionWrapper.tsx
+++ src/app/components/SectionWrapper.tsx
@@ -8,9 +8,9 @@ export function SectionWrapper({ id, children }: SectionWrapperProps) {
     return (
         <section
             id={id}
-            className="w-full py-16 px-6 md:px-12 lg:px-24 bg-background text-foreground"
+            className="w-full py-10 px-6 md:px-12 lg:px-24 bg-background text-foreground"
         >
-            <div className="mx-auto max-w-4xl">{children}</div>
+            <div className="mx-auto max-w-7xl">{children}</div>
         </section>
     );
 }
--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -2,52 +2,54 @@ import { HERO } from "@/lib/constant";
 import { Github01FreeIcons, Linkedin01FreeIcons, Mail01FreeIcons } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
 import Avatar from "../components/Avatar";
+import { SectionWrapper } from "../components/SectionWrapper";
 
 export default function Hero() {
     return (
-        <section id="hero" className="min-h-screen flex flex-col md:flex-row items-center justify-between px-6 md:px-20 py-16 bg-black text-white">
-            {/* Left content */}
-            <div className="flex-1 flex flex-col gap-6 max-w-xl text-center md:text-left justify-center">
-                <h1 className="text-4xl font-bold">
-                    {HERO.name}
-                </h1>
-                <p className="text-lg text-gray-300">
-                    {HERO.summary}
-                </p>
+        <SectionWrapper id="hero">
+            <div className="flex flex-col md:flex-row items-center justify-between gap-0 md:gap-0 min-h-screen">
+                <div className="flex-1 flex flex-col gap-6 max-w-xl text-center md:text-left justify-center">
+                    <h1 className="text-4xl font-bold">
+                        {HERO.name}
+                    </h1>
+                    <p className="text-lg text-gray-300">
+                        {HERO.summary}
+                    </p>
 
-                <a
-                    href="#projects"
-                    className="inline-block px-6 py-3 bg-primary text-black font-semibold rounded-lg shadow-md hover:bg-primary-dark transition text-center"
-                >
-                    View Projects
-                </a>
+                    <a
+                        href="#projects"
+                        className="inline-block px-6 py-3 bg-primary text-black font-semibold rounded-lg shadow-md hover:bg-primary-dark transition text-center"
+                    >
+                        View Projects
+                    </a>
 
-                <div className="flex justify-center md:justify-start gap-6 text-gray-400">
-                    <a href="https://github.com/umuieme" aria-label="GitHub" target="_blank" rel="noopener noreferrer">
-                        <HugeiconsIcon
-                            icon={Github01FreeIcons}
-                            size={24}
-                        />
-                    </a>
-                    <a href="https://linkedin.com/in/umuieme" aria-label="LinkedIn" target="_blank" rel="noopener noreferrer">
-                        <HugeiconsIcon
-                            icon={Linkedin01FreeIcons}
-                            size={24}
-                        />
-                    </a>
-                    <a href="mailto:basnet2umesh@gmail.com" aria-label="Email">
-                        <HugeiconsIcon
-                            icon={Mail01FreeIcons}
-                            size={24}
-                        />
-                    </a>
-                </div>
-            </div >
+                    <div className="flex justify-center md:justify-start gap-6 text-gray-400">
+                        <a href="https://github.com/umuieme" aria-label="GitHub" target="_blank" rel="noopener noreferrer">
+                            <HugeiconsIcon
+                                icon={Github01FreeIcons}
+                                size={24}
+                            />
+                        </a>
+                        <a href="https://linkedin.com/in/umuieme" aria-label="LinkedIn" target="_blank" rel="noopener noreferrer">
+                            <HugeiconsIcon
+                                icon={Linkedin01FreeIcons}
+                                size={24}
+                            />
+                        </a>
+                        <a href="mailto:basnet2umesh@gmail.com" aria-label="Email">
+                            <HugeiconsIcon
+                                icon={Mail01FreeIcons}
+                                size={24}
+                            />
+                        </a>
+                    </div>
+                </div >
 
-            {/* Right avatar */}
-            <div className="flex flex-1 mt-10 md:mt-0 md:ml-10 justify-center" >
-                <Avatar />
-            </div >
-        </section >
+                {/* Right avatar */}
+                <div className="flex flex-1 mt-10 md:mt-0 md:ml-10 justify-center">
+                    <Avatar />
+                </div >
+            </div>
+        </SectionWrapper>
     );
 }
